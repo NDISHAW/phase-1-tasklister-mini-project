271b71705677b956d98596d42ff2a0f85a6e2e27
NDICHU <ndichumuriithi@gmail.com>
2022-04-20 15:21:07 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener(`DOMContentLoaded`, () => {
+  let form=document.querySelector(`form`)
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    buildTasks(e.target.newTaskDescription.value)
+    form.reset()
+  })
+})
+
+function buildTasks(tasks){
+  console.log(tasks);
+  let p= document.createElement(`p`)
+  let btn=document.createElement(`button`)
+  btn.addEventListener(`click`, handlesDelete)
+  btn.textContent= `X  `
+  p.textContent=`${tasks   }    `
+  p.appendChild(btn)
+  console.log(p);
+  document.querySelector(`#tasks`).appendChild(p)
+}
+
+function handlesDelete(e) {
+  e.target.parentNode.remove()
+}
